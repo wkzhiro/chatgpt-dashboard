87693b6cc1b6036ab6eaf9f3119c1e6ee9ce9f8e
wkzhiro <wkzhiro0116@yahoo.co.jp>
2024-08-17 03:01:22 +0900

    csvダウンロードの追加

--- plot/urls.py
+++ plot/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+app_name = 'plot'
 urlpatterns = [
     path("", views.ChartsView.as_view(), name="plot"),
+    path("export/", views.csv_export, name="csv_export"),#追記
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- plot/views.py
+++ plot/views.py
@@ -12,24 +12,11 @@ from collections import defaultdict
 import pandas as pd
 import collections
 
-
-
-# WordCloudの画像を保存する
-def save_wordcloud_image(wordcloud):
-    # BytesIOオブジェクトに画像を保存
-    buffer = BytesIO()
-    wordcloud.to_image().save(buffer, format='PNG')
-    image_content = buffer.getvalue()
-
-    # Djangoのファイルストレージに保存
-    image_file_name = 'wordcloud_image.png'
-    file_path = os.path.join(settings.MEDIA_ROOT, image_file_name)
-
-    # ファイルに保存
-    with open(file_path, 'wb') as f:
-        f.write(image_content)
-
-    return file_path
+# from plot.csv import csvdownload
+from django.http import HttpResponse
+import csv
+import io
+import urllib.request
 
 # .envファイルを読み込む
 load_dotenv()
@@ -52,16 +39,26 @@ for item in items:
         month_year = unix_timestamp_to_month(ts)
         monthly_summary[month_year] += 1
 
-#wordcloudの作成
+#wordcloudの作成と会話データの取得
 question_list =[]
+csv_list = []
 
 for item in items:
     messages = item.get('messages')
+    category = item.get('category')
+
     if messages:
         question = messages[0]["content"]
         question_list.append(question)
+        if category:
+            csv_list.append([",".join(category), question])
+        else:
+            csv_list.append(["", question])
 
 wordcloud_image_path = generate_and_save_wordcloud(question_list)
+
+
+
 
 class ChartsView(TemplateView):  # ❶
     template_name = "plot.html"
@@ -73,3 +70,15 @@ class ChartsView(TemplateView):  # ❶
         # 画像のURLをコンテキストに追加
         context["wordcloud_image_url"] = os.path.join(settings.MEDIA_URL, wordcloud_image_path)
         return context
+
+
+def csv_export(request):
+    response = HttpResponse(content_type='text/csv; charset=utf-8')
+    filename = urllib.parse.quote(('data.csv'))
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    # Add BOM to the response to help Excel recognize UTF-8 encoding
+    response.write('\ufeff'.encode('utf-8'))
+    writer = csv.writer(response)
+    writer.writerow(['カテゴリ', '会話'])
+    writer.writerows(csv_list)
+    return response
